40cb4519e2686e63413bd27e626337180c3faaae
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-12 15:15:18 +0000

    Update camada de dados.h

--- camada de dados.h
+++ camada de dados.h
@@ -21,3 +21,6 @@ int jogador_atual;
 } ESTADO;
 
 ESTADO *inicializar_estado();
+int obter_jogador_atual(ESTADO *estado);
+int obter_numero_de_jogadas(ESTADO *estado);
+CASA obter_estado_casa(ESTADO *e, COORDENADA c);

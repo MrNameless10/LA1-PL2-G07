f781386f1a7c9c8e34966d1b21665bdf8ccb693f
pedro74gomes <62063732+pedro74gomes@users.noreply.github.com>
2020-03-12 18:17:14 +0000

    Update logica do programa.c

--- logica do programa.c
+++ logica do programa.c
@@ -1,9 +1,21 @@
 // Função que deve ser completada e colocada na camada da lógica do programa
+int ver_movimentos(ESTADO*e, COORDENADA c){
+    int c1,l1,c2,l2;
+    c1 = e-> ultima_jogada.x;
+    l1 = e-> ultima_jogada.y;
+    c2 = c.x;
+    l2 = c.y;
+    
+    if((sqrt((c1-c2)^2-(l1-l2)^2) == 1
+        return 1;
+}
+        
 int jogar(ESTADO *e, COORDENADA c) {
-printf("jogar %d %d\n", c.coluna, c.linha);
-return 1;
+    printf("jogar %d %d\n", c.coluna, c.linha);
+    if(ver_movimentos(e,c)){
+     e-> tab[c.x][c.y] = BRANCA;
+     e-> tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
+        return 1;}
+    else return 0;    
 }
-CASA obter_estado_casa(ESTADO *e, COORDENADA c){
-    tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
-    tab[c.x][c.y] = BRANCA;
-}
+

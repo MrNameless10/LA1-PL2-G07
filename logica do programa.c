fff7711e32f1b10b1bb3f80d0c98439fa89f43f8
pedro74gomes <62063732+pedro74gomes@users.noreply.github.com>
2020-03-12 18:31:05 +0000

    Update logica do programa.c

--- logica do programa.c
+++ logica do programa.c
@@ -1,4 +1,6 @@
-// Função que deve ser completada e colocada na camada da lógica do programa
+//
+// Created by pedro on 12/03/2020.
+//
 int ver_movimentos(ESTADO*e, COORDENADA c){
     int c1,l1,c2,l2;
     c1 = e-> ultima_jogada.x;
@@ -13,19 +15,13 @@ int ver_movimentos(ESTADO*e, COORDENADA c){
 int jogar(ESTADO *e, COORDENADA c) {
     printf("jogar %d %d\n", c.coluna, c.linha);
     if(ver_movimentos(e,c)){
-     e-> tab[c.x][c.y] = BRANCA;
-     e-> tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
-        return 1;}
-    else return 0;    
-}
-
-int jogar(ESTADO *e, COORDENADA c){
-    
-    e->tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
+     e->tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
 	e->tab[c.x][c.y] = BRANCA;
     e->ultima_jogada.x = c.x;
     e->ultima_jogada.y = c.y;	
     e->jogador_atual = obter_jogador_atual (e);
     e->jogadas[e->obter_numero_de_jogadas(e)] = c;
+        return 1;}
+    else return 0;    
 }
 

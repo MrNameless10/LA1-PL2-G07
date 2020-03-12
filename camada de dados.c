ef143baffab916c18a573c06ffd00fd2d02a4a46
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-12 16:25:05 +0000

    Update camada de dados.c

--- camada de dados.c
+++ camada de dados.c
@@ -1,18 +1,18 @@
 ESTADO *inicializar_estado() {
     ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
     int x,y;
-    for(y=8;y>=1;y--) {
-        for (x = 1; x <= 8; x++) {
+    for(y=0;y<8;y++) {
+        for (x=0; x<8; x++) {
             e->tab[x][y] = VAZIO;
         }
     }
-    e->ultima_jogada.x = 5;
-    e->ultima_jogada.y = 5;
-    e->tab[5][5] = BRANCA;
-    e->num_jogadas = 0;
+    e->ultima_jogada.x = 4;
+    e->ultima_jogada.y = 3; 
+    e->tab[4][3] = BRANCA; //posicao da peca branca no inicio do jogo
+    e->num_jogadas = 0; 
     e->jogador_atual = 1;
     //e->jogadas
-    
+
     return e;
 }
 //int obter_jogador_atual(ESTADO *estado){

102a27e5d87c5c5f9a3fd416f035f16e384fffb7
MrNameless10 <61701003+MrNameless10@users.noreply.github.com>
2020-03-12 17:50:25 +0000

    Update logica do programa.c

--- logica do programa.c
+++ logica do programa.c
@@ -3,3 +3,7 @@ int jogar(ESTADO *e, COORDENADA c) {
 printf("jogar %d %d\n", c.coluna, c.linha);
 return 1;
 }
+CASA obter_estado_casa(ESTADO *e, COORDENADA c){
+    tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
+    tab[c.x][c.y] = BRANCA;
+}

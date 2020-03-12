3a36c51c0546f4643503fe03bb65a403963e13d2
pedro74gomes <62063732+pedro74gomes@users.noreply.github.com>
2020-03-12 18:31:59 +0000

    Update logica do programa.c

--- logica do programa.c
+++ logica do programa.c
@@ -15,12 +15,12 @@ int ver_movimentos(ESTADO*e, COORDENADA c){
 int jogar(ESTADO *e, COORDENADA c) {
     printf("jogar %d %d\n", c.coluna, c.linha);
     if(ver_movimentos(e,c)){
-     e->tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
-	e->tab[c.x][c.y] = BRANCA;
-    e->ultima_jogada.x = c.x;
-    e->ultima_jogada.y = c.y;	
-    e->jogador_atual = obter_jogador_atual (e);
-    e->jogadas[e->obter_numero_de_jogadas(e)] = c;
+	    e->tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
+	    e->tab[c.x][c.y] = BRANCA;
+	    e->ultima_jogada.x = c.x;
+	    e->ultima_jogada.y = c.y;	
+	    e->jogador_atual = obter_jogador_atual (e);
+	    e->jogadas[e->obter_numero_de_jogadas(e)] = c;
         return 1;}
     else return 0;    
 }

d1936efb69d4acc8b902e4b53528be989e8b2f34
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-12 15:59:38 +0000

    Update camada de dados.c

--- camada de dados.c
+++ camada de dados.c
@@ -1,16 +1,22 @@
 ESTADO *inicializar_estado() {
     ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
     int x,y;
-    for(y=8;y>=1;y--){
-        for(x=1;x<=8;x++){
+    for(y=8;y>=1;y--) {
+        for (x = 1; x <= 8; x++) {
             e->tab[x][y] = VAZIO;
         }
     }
+    e->ultima_jogada.x = 5;
+    e->ultima_jogada.y = 5;
     e->tab[5][5] = BRANCA;
     e->num_jogadas = 0;
     e->jogador_atual = 1;
-    //e->ultima_jogada
     //e->jogadas
-
+    
     return e;
 }
+//int obter_jogador_atual(ESTADO *estado){
+
+//int obter_numero_de_jogadas(ESTADO *estado){
+
+//CASA obter_estado_casa(ESTADO *e, COORDENADA c){

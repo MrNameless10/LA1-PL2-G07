3df16907dd9a5416dd8d52b1add8a225534019a9
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-12 15:00:40 +0000

    Update camada de interface.c

--- camada de interface.c
+++ camada de interface.c
@@ -1,5 +1,16 @@
 void mostrar_tabuleiro(ESTADO *e) {
+    int letra,numero;
+    for(numero=8;numero>=1; numero--){
+        for(letra='a';letra<='h';letra++){
+            if(letra== 'h' && numero==8) printf("2");
+            else if (letra=='a' && numero==1) printf("1");
+            else if (letra=='e'&& numero==5) printf("*");
+            else printf(".");
+        }
+        printf("\n");
+    }
 }
+
 
 // Função que deve ser completada e colocada na camada de interface
 int interpretador(ESTADO *e) {

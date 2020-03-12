39cefaee311444c4b68738588dcdc62a615f1393
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-12 16:23:59 +0000

    Update camada de interface.c

--- camada de interface.c
+++ camada de interface.c
@@ -1,18 +1,17 @@
 void mostrar_tabuleiro(ESTADO *e) {
     int x ,y;
 
-    for(y=8;y>=1; y--){
-        for(x=1;x<=8;x++){
-            if(x==8 && y==8) printf("2");
-            else if (x==1 && y==1) printf("1");
+    for(y=0;y<8; y++){//0 a 7 por 0 a 7. A posicao (0,0) equivale a primeira casa no canto superior esquerdo.
+        for(x=0;x<8;x++){
+            if (x==7 && y==0) printf("2");
+            else if (x==0 && y==7)printf("1");
             else if (e->tab[x][y]==BRANCA) printf("*");
-            else if (e->tab[x][y]==VAZIO) printf(".");
             else if (e->tab[x][y]==PRETA) printf("#");
+            else if (e->tab[x][y]==VAZIO) printf(".");
         }
         printf("\n");
     }
 }
-
 
 // Função que deve ser completada e colocada na camada de interface
 int interpretador(ESTADO *e) {

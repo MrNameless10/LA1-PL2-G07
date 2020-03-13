61cb8e3516b0ad1cf2e3e6b5f62ff966a73c088a
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-13 14:16:49 +0000

    Update logica do programa.c

--- logica do programa.c
+++ logica do programa.c
@@ -1,6 +1,7 @@
 //
 // Created by pedro on 12/03/2020.
 //
+#include "camada de dados.h"
 int ver_movimentos(ESTADO*e, COORDENADA c){
     int c1,l1,c2,l2;
     c1 = e-> ultima_jogada.x;
@@ -24,4 +25,3 @@ int jogar(ESTADO *e, COORDENADA c) {
         return 1;}
     else return 0;    
 }
-

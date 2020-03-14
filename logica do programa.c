a04aa369700cbf5b266911ed338dc34d59126184
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-14 15:37:19 +0000

    Update logica do programa.c

--- logica do programa.c
+++ logica do programa.c
@@ -1,26 +1,25 @@
-
 #include "camada de dados.h"
 #include <math.h>
 #include <stdio.h>
 
 int ver_movimentos(ESTADO*e, COORDENADA c){
     int c1,l1,c2,l2;
-    c1 = e-> ultima_jogada.linha;
-    l1 = e-> ultima_jogada.coluna;
-    c2 = c.linha;
-    l2 = c.coluna;
+    c1 = obter_ultima_jogada(e).x
+    l1 = obter_ultima_jogada(e).y
+    c2 = c.x;
+    l2 = c.y;
     
     if((sqrt((c1-c2)^2-(l1-l2)^2) == 1
         return 1;
 }
         
 int jogar(ESTADO *e, COORDENADA c) {
-    printf("jogar %d %d\n", c.coluna, c.linha);
+    printf("jogar %d %d\n", c.x, c.y);
     if(ver_movimentos(e,c)){
 	    e->tab[ultima_jogada.x][ultima_jogada.y] = PRETA;
-	    e->tab[c.linha][c.coluna] = BRANCA;
-	    e->ultima_jogada.linha = c.linha;
-	    e->ultima_jogada.coluna = c.coluna;	
+	    e->tab[c.x][c.y] = BRANCA;
+	    e->ultima_jogada.x = c.x;
+	    e->ultima_jogada.y = c.y;	
 	    e->jogador_atual = obter_jogador_atual (e);
 	    e->jogadas[e->obter_numero_de_jogadas(e)] = c;
         return 1;}

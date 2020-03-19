e2c12a8ce28cc81555f5b9cd713ac23e188a26b9
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-19 15:35:28 +0000

    Update camada de interface.c

--- camada de interface.c
+++ camada de interface.c
@@ -6,16 +6,9 @@
 #define BUF_SIZE 1024
 
 void mostrar_tabuleiro(ESTADO *e) {
-    int x ,y;
+    int x ,y,i=8;
     for(y=0;y<8; y++){
-        if(y==0) printf("8 ");
-        if(y==1) printf("7 ");
-        if(y==2) printf("6 ");
-        if(y==3) printf("5 ");
-        if(y==4) printf("4 ");
-        if(y==5) printf("3 ");
-        if(y==6) printf("2 ");
-        if(y==7) printf("1 ");
+        printf("%d ", i);
         for(x=0;x<8;x++){
             if (x==7 && y==0) printf("2");
             else if (x==0 && y==7)printf("1");
@@ -24,6 +17,7 @@ void mostrar_tabuleiro(ESTADO *e) {
             else if (obter_casa(e,x,y)==VAZIO) printf(".");
         }
         printf("\n");
+        i--;
     }
     printf("  abcdefgh");
     printf("\n");

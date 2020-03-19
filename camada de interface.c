9a0b51d6cf1191c73ae9dc2c7f6e5b147b0d8e9b
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-19 15:33:11 +0000

    Update camada de interface.c

--- camada de interface.c
+++ camada de interface.c
@@ -8,6 +8,14 @@
 void mostrar_tabuleiro(ESTADO *e) {
     int x ,y;
     for(y=0;y<8; y++){
+        if(y==0) printf("8 ");
+        if(y==1) printf("7 ");
+        if(y==2) printf("6 ");
+        if(y==3) printf("5 ");
+        if(y==4) printf("4 ");
+        if(y==5) printf("3 ");
+        if(y==6) printf("2 ");
+        if(y==7) printf("1 ");
         for(x=0;x<8;x++){
             if (x==7 && y==0) printf("2");
             else if (x==0 && y==7)printf("1");
@@ -17,9 +25,11 @@ void mostrar_tabuleiro(ESTADO *e) {
         }
         printf("\n");
     }
+    printf("  abcdefgh");
+    printf("\n");
 }
 
-// Funcao INCOMPLETA
+
 int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
     char col[2], lin[2];

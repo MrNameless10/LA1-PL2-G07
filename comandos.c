9aeb68cd02331f2cefb3da297be3b2c740b8d0e5
pedro74gomes <62063732+pedro74gomes@users.noreply.github.com>
2020-03-19 16:39:36 +0000

    Create comandos.c

---
+++ comandos.c
@@ -0,0 +1,13 @@
+int ler(){
+    FILE *fp1;char c;
+    fp1= fopen ("C:\\meusficheiros \\ novoficheiro.txt", "ler");
+    while(1){
+        c = fgetc(fp1);
+        if(c==EOF)
+            break;
+        else
+            printf("%c", c);
+    }
+    fclose(fp1);
+    return 0;
+}

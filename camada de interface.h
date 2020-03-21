861db361f2f994d0c9962df133e43f080f2c7cf2
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-21 19:40:59 +0000

    Update camada de interface.h

--- camada de interface.h
+++ camada de interface.h
@@ -1,12 +1,3 @@
-//
-// Created by miguel on 18/03/2020.
-//
-
-#ifndef LAPROJETO_CAMADA_DE_INTERFACE_H
-#define LAPROJETO_CAMADA_DE_INTERFACE_H
-
 #include "camada de dados.h"
 void mostrar_tabuleiro(ESTADO *e);
 int interpretador(ESTADO *e);
-
-#endif //LAPROJETO_CAMADA_DE_INTERFACE_H

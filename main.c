ac284b7c4f7689ca85e5e307a3a6a318eac17497
Tomás Campinho <61989193+TomasCampinho@users.noreply.github.com>
2020-03-21 15:45:54 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include "camada de dados.h"
-#include "camada de interface.h"
-#include "logica do programa.h"
+#include "camada_de_dados.h"
+#include "camada_de_interface.h"
+#include "logica_do_programa.h"
 
 int main() {
     ESTADO *e = inicializar_estado();

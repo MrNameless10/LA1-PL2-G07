17dcc3922d8b6f1c8388d0528ce2ae305cb8c9ad
MrNameless10 <miguelfreitas79@hotmail.com>
2020-03-21 16:22:53 +0000

    UPDATE FIX

--- camada de interface.h
+++
@@ -1,3 +0,0 @@
-#include "camada de dados.h"
-void mostrar_tabuleiro(ESTADO *e);
-int interpretador(ESTADO *e);
--- main.c
+++
@@ -1,9 +0,0 @@
-#include "camada de dados.h"
-#include "camada de interface.h"
-#include "logica do programa.h"
-
-int main(){
-ESTADO *e = inicializar_estado();
-interpretador(e);
-return 0;
-}

4d7ffeb87556de92dd4a63d00694ff81bd2fce1c
MrNameless10 <miguelfreitas79@hotmail.com>
2020-03-22 18:35:13 +0000

    Making the best of you

---
+++ cmake-build-debug/CMakeFiles/LA1_PL2_G07.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/migue/Documents/GitHub/LA1-PL2-G07/LA1-PL2-G07/camada de dados.c" "C:/Users/migue/Documents/GitHub/LA1-PL2-G07/LA1-PL2-G07/cmake-build-debug/CMakeFiles/LA1_PL2_G07.dir/camada_de_dados.c.obj"
+  "C:/Users/migue/Documents/GitHub/LA1-PL2-G07/LA1-PL2-G07/camada de interface.c" "C:/Users/migue/Documents/GitHub/LA1-PL2-G07/LA1-PL2-G07/cmake-build-debug/CMakeFiles/LA1_PL2_G07.dir/camada_de_interface.c.obj"
+  "C:/Users/migue/Documents/GitHub/LA1-PL2-G07/LA1-PL2-G07/logica de programa.c" "C:/Users/migue/Documents/GitHub/LA1-PL2-G07/LA1-PL2-G07/cmake-build-debug/CMakeFiles/LA1_PL2_G07.dir/logica_de_programa.c.obj"
+  "C:/Users/migue/Documents/GitHub/LA1-PL2-G07/LA1-PL2-G07/main.c" "C:/Users/migue/Documents/GitHub/LA1-PL2-G07/LA1-PL2-G07/cmake-build-debug/CMakeFiles/LA1_PL2_G07.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/LA1_PL2_G07.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LA1_PL2_G07.dir/camada_de_dados.c.obj"
+  "CMakeFiles/LA1_PL2_G07.dir/camada_de_interface.c.obj"
+  "CMakeFiles/LA1_PL2_G07.dir/logica_de_programa.c.obj"
+  "CMakeFiles/LA1_PL2_G07.dir/main.c.obj"
+  "LA1_PL2_G07.exe"
+  "LA1_PL2_G07.exe.manifest"
+  "LA1_PL2_G07.pdb"
+  "libLA1_PL2_G07.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LA1_PL2_G07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

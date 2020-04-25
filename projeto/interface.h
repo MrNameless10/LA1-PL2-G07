49a863418f1ff1321648f004833fab8ebf8978c4
pedro74gomes <62063732+pedro74gomes@users.noreply.github.com>
2020-04-25 16:24:05 +0100

    Update interface.h

--- projeto/interface.h
+++ projeto/interface.h
@@ -5,32 +5,32 @@ Definições das funções que fazem a ligação ao jogador
 #include "dados.h"
 
 /**
-\Mostra o tabuleiro na linha de comandos @param e aponta para o estado do jogo
+\brief Mostra o tabuleiro na linha de comandos @param e aponta para o estado do jogo
 */
 void mostrar_tabuleiro(ESTADO *e);
 
 /**
-\Recebe o input do jogador @param e aponta para o estado do jogo
+\brief Recebe o input do jogador @param e aponta para o estado do jogo
 */
 int interpretador(ESTADO *e);
 
 //void q ();
 /**
-/Lê um ficheiro @param ficheiro @param e aponta para o Estado do jogo
+/brief Lê um ficheiro @param ficheiro @param e aponta para o Estado do jogo
 */
 void ler(char *ficheiro, ESTADO *e);
 
 /**
-/Grava um ficheiro @param ficheiro @param e aponta para o Estado do jogo
+/brief Grava um ficheiro @param ficheiro @param e aponta para o Estado do jogo
 */
 void gr(char *ficheiro, ESTADO *e);
 
 /**
-\Imprime o prompt @param e aponta para o estado do jogo
+\brief Imprime o prompt @param e aponta para o estado do jogo
 */
 void mostrar_prompt(ESTADO *e);
 
 /**
-\Mostra o tabuleiro na linha de comandos @param e aponta para o estado do jogo @param guarda o jogo no File para o output
+\brief Mostra o tabuleiro na linha de comandos @param e aponta para o estado do jogo @param guarda o jogo no File para o output
 */
 void escreve_tabuleiro(ESTADO *e, FILE *jogo);
